db794f503803c99c58c6af2907f0f8ddc082d058
Sean Ni <nixiang915@hotmail.com>
2023-03-17 22:12:09 -0400

    Add working example

--- octoprint_octopager/__init__.py
+++ octoprint_octopager/__init__.py
@@ -1,25 +1,15 @@
 # coding=utf-8
 from __future__ import absolute_import
-
-# (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
 
 class OctopagerPlugin(octoprint.plugin.StartupPlugin,
                       octoprint.plugin.SettingsPlugin,
-                      #   octoprint.plugin.AssetPlugin,
+                      octoprint.plugin.AssetPlugin,
                       octoprint.plugin.TemplatePlugin
                       ):
 
     def on_after_startup(self):
-        self._logger.info("Hello OctoPager 123!!!")
         self._logger.info("Hello World! (more: %s)" %
                           self._settings.get(["url"]))
 
@@ -28,41 +18,44 @@ class OctopagerPlugin(octoprint.plugin.StartupPlugin,
     def get_settings_defaults(self):
         return dict(url="https://en.wikipedia.org/wiki/Hello_world")
 
-    def get_template_vars(self):
-        return dict(url=self._settings.get(["url"]))
-
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False)
+        ]
+    
     # ~~ AssetPlugin mixin
 
-    # def get_assets(self):
-    #     # Define your plugin's asset files to automatically include in the
-    #     # core UI here.
-    #     return {
-    #         "js": ["js/octopager.js"],
-    #         "css": ["css/octopager.css"],
-    #         "less": ["less/octopager.less"]
-    #     }
+    def get_assets(self):
+        # Define your plugin's asset files to automatically include in the
+        # core UI here.
+        return {
+            "js": ["js/octopager.js"],
+            "css": ["css/octopager.css"],
+            "less": ["less/octopager.less"]
+        }
 
     # # ~~ Softwareupdate hook
 
-    # def get_update_information(self):
-    #     # Define the configuration for your plugin to use with the Software Update
-    #     # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-    #     # for details.
-    #     return {
-    #         "octopager": {
-    #             "displayName": "Octopager Plugin",
-    #             "displayVersion": self._plugin_version,
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+        # for details.
+        return {
+            "octopager": {
+                "displayName": "Octopager Plugin",
+                "displayVersion": self._plugin_version,
 
-    #             # version check: github repository
-    #             "type": "github_release",
-    #             "user": "NX915",
-    #             "repo": "OctoPrint-OctoPager",
-    #             "current": self._plugin_version,
+                # version check: github repository
+                "type": "github_release",
+                "user": "NX915",
+                "repo": "OctoPrint-OctoPager",
+                "current": self._plugin_version,
 
-    #             # update method: pip
-    #             "pip": "https://github.com/NX915/OctoPrint-OctoPager/archive/{target_version}.zip",
-    #         }
-    #     }
+                # update method: pip
+                "pip": "https://github.com/NX915/OctoPrint-OctoPager/archive/{target_version}.zip",
+            }
+        }
 
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
@@ -75,13 +68,12 @@ __plugin_name__ = "OctoPager"
 # OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
 # OctoPrint 1.8.0 onwards only supports Python 3.
 __plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
-__plugin_implementation__ = OctopagerPlugin()
 
-# def __plugin_load__():
-#     global __plugin_implementation__
-#     __plugin_implementation__ = OctopagerPlugin()
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = OctopagerPlugin()
 
-#     global __plugin_hooks__
-#     __plugin_hooks__ = {
-#         # "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-#     }
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        # "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
